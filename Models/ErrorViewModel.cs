390227798863390f6e8fd781bd59a33cc06865da
Mohammad Nojoumy <mohammadnojoumy@gmail.com>
2022-02-01 12:51:07 +0330

    Add project to git

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pagination.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
