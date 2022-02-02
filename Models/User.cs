27cde9fd6eb03433fb4962377742335bc1fad70b
Mohammad Nojoumy <mohammadnojoumy@gmail.com>
2022-02-02 23:58:24 +0330

    Add user.cs entity

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+using System;
+namespace pagination.Models{
+    public class User{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public int Age { get; set; }
+        public DateTime CreateDate { get; set; } 
+        
+    }
+}
